313cd562feb17d713aa0fb0efe2ebe3a45b6b4d9
Dee Lynn <deannalguzman@gmail.com>
2019-09-26 23:31:28 -0700

    added more functions to the start button to hide everything and show the question.

--- script.js
+++ script.js
@@ -1,47 +1,76 @@
+//~~~~~~~~~~~~Variables:~~~~~~~~~~//
+var startbtn = document.getElementById("buttonStart");
+var quesArea = document.getElementById("questionsArea");
+var starting = document.querySelector(".startPage");
+var ansArea = document.querySelector(".answersArea");
+var timer = document.getElementById("timer");
+var results = document.getElementById("results");
+var nextbtn = document.getElementById("nextbtn");
+
+//~~~~~~~Questions:~~~~~~~~~~~~~~//
 var questions = [
     {
       title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
+      choiceA: "strings", 
+      choiceB: "booleans", 
+      choiceC: "alerts", 
+      choiceD: "numbers",
+      answer: "C"
     },
     {
       title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
+      choiceA: "quotes", 
+      choiceB: "parentheses", 
+      choiceC: "curly brackets",
+      choiceD: "square brackets",
+      answer: "B"
     },
     {
         title: "Inside which HTML element do we put the JavaScript?",
-        choices: ["<js>", "<script>", "<javascript>", "<scripttag>"],
-        answer: "<script>"
+        choiceA: "<js>", 
+        choiceB: "<script>", 
+        choiceC: "<javascript>", 
+        choiceD: "<scripttag>",
+        answer: "B"
     },
   ];
+  
 
-function askQuestion(index) {
-    var questionObj = questions[index];
-    var userAnswer = confirm(questionObj.q);
-    return userAnswer;
-    function inside() {
-        var example = 12;
-    }
+startbtn.addEventListener("click", startTimer);
+startbtn.addEventListener("click", startQuiz);
+nextbtn.addEventListener("click", nextQ);
+
+//~~~~Timer~~~~~~~//
+function startTimer() {
+    var time = 60;
+    var intervalId = setInterval(function() {
+        time--;
+        timer.textContent = time;
+        if (time === 0) {
+            quesArea.innerHTML = " ";
+            ansArea.innerHTML = "<h1>Time is up!</h1>";
+        }
+    }, 1000);
 }
 
-function compareAnswers(index, uAnswer) {
-    var questionObject = questions[index];
-    return uAnswer === questionObject.a;
+function postQuestions() {
+  for (var i = 0; i < questions.length; i++){
+     console.log(questions[i].title);
+        quesArea.textContent = questions[i].title;
+        ansArea.textContent = questions[i].choiceA;
+        ansArea.textContent = questions[i].choiceB;
+        ansArea.textContent = questions[i].choiceC;
+        ansArea.textContent = questions[i].choiceD;
+        nextQ();
+}
 }
 
-function testUser(index) {
-    var answer = askQuestion(index);
-    var isCorrect = compareAnswers(index, answer);
-    if (isCorrect) {
-        alert("Correct!");
-    } else {
-        alert("Incorrect!");
-    }
+function nextQ (){
+    nextbtn.innerHTML = "<h4>Next Question</h4>";
+
 }
 
-var btn = document.getElementById("buttonStart");
-
-btn.addEventListener("click", function(){
-    
-})
+function startQuiz() {
+    starting.hidden = true;
+    postQuestions();
+}
