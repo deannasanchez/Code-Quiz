5413a37da66d1f4c149c74276f076f95573cd554
Dee Lynn <deannalguzman@gmail.com>
2019-09-27 20:35:01 -0700

    tried to make the answers buttons, didn't work. created an if function for the answers

--- script.js
+++ script.js
@@ -6,32 +6,25 @@ var ansArea = document.querySelector(".answersArea");
 var timer = document.getElementById("timer");
 var results = document.getElementById("results");
 var nextbtn = document.getElementById("nextbtn");
+var currentQuestion = 0;
+var intervalId;
 
-//~~~~~~~Questions:~~~~~~~~~~~~~~//
+//~~~~~~~~~~Questions:~~~~~~~~~~//
 var questions = [
     {
       title: "Commonly used data types DO NOT include:",
-      choiceA: "strings", 
-      choiceB: "booleans", 
-      choiceC: "alerts", 
-      choiceD: "numbers",
-      answer: "C"
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
     },
     {
       title: "The condition in an if / else statement is enclosed within ____.",
-      choiceA: "quotes", 
-      choiceB: "parentheses", 
-      choiceC: "curly brackets",
-      choiceD: "square brackets",
-      answer: "B"
+      choices: ["quotes", "parentheses", "curly brackets", "square brackets"],
+      answer: "parentheses"
     },
     {
         title: "Inside which HTML element do we put the JavaScript?",
-        choiceA: "<js>", 
-        choiceB: "<script>", 
-        choiceC: "<javascript>", 
-        choiceD: "<scripttag>",
-        answer: "B"
+        choices: ["<js>", "<script>", "<javascript>", "<scripttag>"],
+        answer: "<script>"
     },
   ];
   
@@ -43,34 +36,51 @@ nextbtn.addEventListener("click", nextQ);
 //~~~~Timer~~~~~~~//
 function startTimer() {
     var time = 60;
-    var intervalId = setInterval(function() {
+    intervalId = setInterval(function() {
         time--;
         timer.textContent = time;
         if (time === 0) {
             quesArea.innerHTML = " ";
             ansArea.innerHTML = "<h1>Time is up!</h1>";
+            nextbtn.innerHTML = " ";
+            clearInterval(intervalId);
+            
         }
     }, 1000);
 }
 
+
+//~~~~~~~~Quiz~~~~~~~//
+
 function postQuestions() {
-  for (var i = 0; i < questions.length; i++){
-     console.log(questions[i].title);
-        quesArea.textContent = questions[i].title;
-        ansArea.textContent = questions[i].choiceA;
-        ansArea.textContent = questions[i].choiceB;
-        ansArea.textContent = questions[i].choiceC;
-        ansArea.textContent = questions[i].choiceD;
-        nextQ();
+    quesArea.textContent = questions[currentQuestion].title;
+    var answersStr = "";
+  for (var i = 0; i < questions[currentQuestion].choices.length; i++){
+       
+        answersStr += "<button class='answer-button' id='button' data-name='" + questions[currentQuestion].choices[i]
+            + "'>" + questions[currentQuestion].choices[i] + "</button>";
+        
 }
-}
-
-function nextQ (){
-    nextbtn.innerHTML = "<h4>Next Question</h4>";
-
+ansArea.innerHTML = answersStr;
+nextQ();
 }
 
 function startQuiz() {
     starting.hidden = true;
     postQuestions();
 }
+
+function nextQ (){
+    nextbtn.innerHTML = "Next Question";
+
+}
+
+function compareAs (){
+    if (questions.choices[i] == questions.answer[i]){
+        results.innerHTML = "You answered correctly!";
+    } else {
+        results.innerHTML = "Wrong!";
+    }
+
+}
+
